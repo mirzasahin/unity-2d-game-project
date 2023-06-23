9e2d4c5d41d14556ee9d6058491eecf0aa5b7a13
Mirza Sahin <salihmirza123@gmail.com>
2023-06-23 21:54:48 +0300

    Add character animations

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -11,6 +11,6 @@ public class CameraFollow : MonoBehaviour
     void Update()
     {
         Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
-        transform.position = Vector3.Lerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, newPos, FollowSpeed);
     }
 }
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,35 +6,50 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Transform player;
-    private float dirX;
+    private Animator anim;
+    private float dirX = 0f;
+    private SpriteRenderer sprite;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 10f;
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         player = GetComponent<Transform>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         dirX = Input.GetAxisRaw("Horizontal"); // Player'ın haritadaki konumu, koordinatı. -1 sol, 1 sağ.
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y); // Player'ın hızı.
+
         if(Input.GetKeyDown("space"))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 10f);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+        UpdateAnimationUpdate();
     }
 
-    private void FixedUpdate() {
-        if(dirX < 0)
+    private void UpdateAnimationUpdate()
+    {
+        if(dirX > 0f)
         {
-            player.rotation = Quaternion.Euler(0f, 180f, 0f); 
+            anim.SetBool("Running", true);
+            sprite.flipX = false;
+
         }
-        else if(dirX > 0)
+        else if(dirX < 0f)
         {
-            player.rotation = Quaternion.Euler(0f, 0f, 0f);
+            anim.SetBool("Running", true);
+            sprite.flipX = true;
         }
-        
-        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y); // Player'ın hızı.
+        else
+        {
+            anim.SetBool("Running", false);
+        }
     }
 }
