764e46bfbb83114205f396ee84736d57f9beb7af
Mirza Sahin <salihmirza123@gmail.com>
2023-06-24 00:11:59 +0300

    Optimize codes

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+   [SerializeField] private Transform target;
+
+
+    // Update is called once per frame
+   private void Update()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z );
+    }
+}
